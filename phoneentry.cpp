5d977900218c70b956955aab21c1fcd8a4fbd628
Sejin Kim <kim3@kenyon.edu>
2019-12-17 16:30:55 -0500

    Create phoneentry.cpp

---
+++ phoneentry.cpp
@@ -0,0 +1,12 @@
+#include "PhoneEntry.h"
+
+PhoneEntry::PhoneEntry() {
+}
+
+PhoneEntry::PhoneEntry(string f, string l, string p, string t, string I) {
+    first = f;
+    last = l;
+    phone = p;
+    type = t;
+    ID=I;
+}
